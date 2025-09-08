2681e69a7d24ebeb733ede6cd986a3c907f51306
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-08 12:12:01 +0500

    create OrdersDetailsGrid component and clean the code with formating and removing unwanted code

--- src/pages/orders/OrdersGrid.jsx
+++ src/pages/orders/OrdersGrid.jsx
@@ -1,8 +1,5 @@
-import dayjs from "dayjs";
-import { Fragment, useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { formatMoney } from "../../utils/money";
 import OrdersHeader from "./OrdersHeader";
+import OrdersDetailsGrid from "./OrdersDetailsGrid";
 
 function OrdersGrid({ orders }) {
   return (
@@ -11,49 +8,9 @@ function OrdersGrid({ orders }) {
         {orders.map((order) => {
           return (
             <div key={order.id} className="order-container">
-             <OrdersHeader order = {order} />
+              <OrdersHeader order={order} />
 
-              <div className="order-details-grid">
-                {order.products.map((orderProduct) => {
-                  return (
-                    <Fragment key={orderProduct.product.id}>
-                      <div className="product-image-container">
-                        <img src={orderProduct.product.image} />
-                      </div>
-
-                      <div className="product-details">
-                        <div className="product-name">
-                          {orderProduct.product.name}
-                        </div>
-                        <div className="product-delivery-date">
-                          Arriving on:{" "}
-                          {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
-                            "MMMM D"
-                          )}
-                        </div>
-                        <div className="product-quantity">
-                          Quantity: {orderProduct.quantity}
-                        </div>
-                        <button className="buy-again-button button-primary">
-                          <img
-                            className="buy-again-icon"
-                            src="./src/assets/images/icons/buy-again.png"
-                          />
-                          <span className="buy-again-message">Add to Cart</span>
-                        </button>
-                      </div>
-
-                      <div className="product-actions">
-                        <Link to="/tracking">
-                          <button className="track-package-button button-secondary">
-                            Track package
-                          </button>
-                        </Link>
-                      </div>
-                    </Fragment>
-                  );
-                })}
-              </div>
+              <OrdersDetailsGrid order={order} />
             </div>
           );
         })}
