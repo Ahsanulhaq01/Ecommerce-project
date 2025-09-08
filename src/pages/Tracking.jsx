ff81fd52eba6cdec883f54189afa8276c81e08b3
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-08 16:24:30 +0500

    formated the Tracking page

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -4,49 +4,50 @@ import "./tracking.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
-function Tracking({cart}) {
-
-  const [order, setOrder] = useState(null)
+function Tracking({ cart }) {
+  const [order, setOrder] = useState(null);
   const params = useParams();
-  const {orderId , productId} = params;
+  const { orderId, productId } = params;
 
-  useEffect(()=>{
-    async function getTrackData(){
-      const response = await axios.get(`/api/orders/${orderId}?expand=products`);
-      setOrder(response.data)
-      console.log(response.data.products.productsId)
-      console.log(productId)
+  useEffect(() => {
+    async function getTrackData() {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`
+      );
+      setOrder(response.data);
+      console.log(response.data.products.productsId);
+      console.log(productId);
     }
     getTrackData();
-  },[])
+  }, []);
 
-if(!order){
-  return <div>loading .....</div>
-}
-  const prod = order.products.find(prod => prod.productId === productId);
+  if (!order) {
+    return <div>loading .....</div>;
+  }
+  const prod = order.products.find((prod) => prod.productId === productId);
   return (
     <>
-   
-    <link rel="icon" type="image/svg+xml" href="./images/tracking-favicon.png" />
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="./images/tracking-favicon.png"
+      />
       <title>Tracking</title>
-      <Header cart={cart}/>
+      <Header cart={cart} />
       <div className="tracking-page">
         <div className="order-tracking">
           <a className="back-to-orders-link link-primary" href="orders">
             View all orders
           </a>
 
-          <div className="delivery-date">{dayjs(prod.estimatedDeliveryTimeMs).format('dddd, MMMM D')}</div>
-
-          <div className="product-info">
-            {prod.product.name}
+          <div className="delivery-date">
+            {dayjs(prod.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
 
+          <div className="product-info">{prod.product.name}</div>
+
           <div className="product-info">Quantity: {prod.quantity}</div>
-          <img
-            className="product-image"
-            src={`../../${prod.product.image}`}
-          />
+          <img className="product-image" src={`../../${prod.product.image}`} />
 
           <div className="progress-labels-container">
             <div className="progress-label">Preparing</div>
