83fd17e929a510766f6fbb956c4e9908a6a897f5
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-09 16:34:09 +0500

    Create the link dynamic of Homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,20 +1,28 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 import Header from "../../components/Header";
 import { formatMoney } from "../../utils/money";
 import "./homepage.css";
 import ProductGrid from "./ProductGrid";
-function HomePage({ cart , loadCart}) {
+function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
+  const url = "/api/products/";
+  const searchUrl = `/api/products?search=${search}`;
+  const gethomedata = async (url) => {
+    const response = await axios.get(url);
+    setProducts(response.data);
+  };
 
   useEffect(() => {
-    const gethomedata = async () => {
-    const response = await axios.get("/api/products/");
-      setProducts(response.data);
-    };
-    gethomedata();
-  }, []);
-
+    if (search) {
+      gethomedata(searchUrl);
+    } else {
+      gethomedata(url);
+    }
+  }, [search]);
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="./images/home-favicon.png" />
