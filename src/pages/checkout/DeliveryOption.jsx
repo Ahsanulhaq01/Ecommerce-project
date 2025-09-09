d6f257a34ea10228a56a7b13828715ffdabe87d0
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-09 11:45:03 +0500

    create component for DeliveryOption to further clean the code

---
+++ src/pages/checkout/DeliveryOption.jsx
@@ -0,0 +1,47 @@
+import dayjs from "dayjs";
+import axios from "axios";
+import { formatMoney } from "../../utils/money";
+
+function DeliveryOption({ deliveryOptions, cartItem ,loadCart , loadPaymentSummary}) {
+  return (
+    <>
+      <div className="delivery-options">
+        <div className="delivery-options-title">Choose a delivery option:</div>
+        {deliveryOptions.map((deliveryOption) => {
+          let priceString = "Free Shipping";
+          if (deliveryOption.priceCents > 0) {
+            priceString = `${formatMoney(deliveryOption.priceCents)}-Shipping`;
+          }
+          async function updateDeliveryOptions() {
+            await axios.put(`/api/cart-items/${cartItem.productId}`,{
+                deliveryOptionId : deliveryOption.id,
+            })
+            await loadCart();
+            await loadPaymentSummary();
+          }
+          return (
+            <div key={deliveryOption.id} className="delivery-option" onClick={updateDeliveryOptions}>
+              <input
+                type="radio"
+                className="delivery-option-input"
+                name={`delivery-option-${cartItem.productId}`}
+                checked={deliveryOption.id === cartItem.deliveryOptionId}
+                onChange={()=>{}}
+              />
+              <div>
+                <div className="delivery-option-date">
+                  {dayjs(deliveryOption.estimatedDeliveryTimeMs).format(
+                    "dddd, MMMM D"
+                  )}
+                </div>
+                <div className="delivery-option-price">{priceString}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default DeliveryOption;
