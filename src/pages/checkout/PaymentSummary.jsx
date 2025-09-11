7309761d9c35588dcc403e715712c48861289be3
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-09-11 13:25:08 +0500

    Format the paymentSummary code

--- src/pages/checkout/PaymentSummary.jsx
+++ src/pages/checkout/PaymentSummary.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useNavigate } from "react-router";
 import { formatMoney } from "../../utils/money";
-export function PaymentSummary({ paymentSummary ,loadCart}) {
+export function PaymentSummary({ paymentSummary, loadCart }) {
   const navigate = useNavigate();
   async function createOrder() {
-    await axios.post('/api/orders');
+    await axios.post("/api/orders");
     await loadCart();
-    navigate('/orders')
+    navigate("/orders");
   }
   return (
     <>
@@ -14,42 +14,76 @@ export function PaymentSummary({ paymentSummary ,loadCart}) {
         <div className="payment-summary-title">Payment Summary</div>
         {paymentSummary && (
           <>
-            <div className="payment-summary-row"data-testid="payment-summary-row">
+            <div
+              className="payment-summary-row"
+              data-testid="payment-summary-row"
+            >
               <div>Items ({paymentSummary.totalItems}):</div>
-              <div className="payment-summary-money" data-testid="correct-dollar-amount">
+              <div
+                className="payment-summary-money"
+                data-testid="correct-dollar-amount"
+              >
                 {formatMoney(paymentSummary.productCostCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row" data-testid="payment-summary-row">
+            <div
+              className="payment-summary-row"
+              data-testid="payment-summary-row"
+            >
               <div>Shipping &amp; handling:</div>
-              <div className="payment-summary-money" data-testid="correct-dollar-amount">
+              <div
+                className="payment-summary-money"
+                data-testid="correct-dollar-amount"
+              >
                 {formatMoney(paymentSummary.shippingCostCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row subtotal-row" data-testid="payment-summary-row">
+            <div
+              className="payment-summary-row subtotal-row"
+              data-testid="payment-summary-row"
+            >
               <div>Total before tax:</div>
-              <div className="payment-summary-money" data-testid="correct-dollar-amount">
+              <div
+                className="payment-summary-money"
+                data-testid="correct-dollar-amount"
+              >
                 {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row" data-testid="payment-summary-row">
+            <div
+              className="payment-summary-row"
+              data-testid="payment-summary-row"
+            >
               <div>Estimated tax (10%):</div>
-              <div className="payment-summary-money" data-testid="correct-dollar-amount">
+              <div
+                className="payment-summary-money"
+                data-testid="correct-dollar-amount"
+              >
                 {formatMoney(paymentSummary.taxCents)}
               </div>
             </div>
 
-            <div className="payment-summary-row total-row" data-testid="payment-summary-row">
+            <div
+              className="payment-summary-row total-row"
+              data-testid="payment-summary-row"
+            >
               <div>Order total:</div>
-              <div className="payment-summary-money" data-testid="correct-dollar-amount">
+              <div
+                className="payment-summary-money"
+                data-testid="correct-dollar-amount"
+              >
                 {formatMoney(paymentSummary.totalCostCents)}
               </div>
             </div>
 
-            <button className="place-order-button button-primary" onClick={createOrder}>
+            <button
+              className="place-order-button button-primary"
+              data-testid="place-order-button"
+              onClick={createOrder}
+            >
               Place your order
             </button>
           </>
--- src/pages/checkout/PaymentSummary.test.jsx
+++ src/pages/checkout/PaymentSummary.test.jsx
@@ -1,27 +1,40 @@
-import { MemoryRouter } from 'react-router'
-import {it,describe,expect,vi,beforeEach} from 'vitest'
-import { PaymentSummary } from './PaymentSummary'
-import { screen,within ,render } from '@testing-library/react'
+import { MemoryRouter } from "react-router";
+import { it, describe, expect, vi, beforeEach } from "vitest";
+import { PaymentSummary } from "./PaymentSummary";
+import { screen, within, render } from "@testing-library/react";
 
-
-describe('PaymmentSummary Component',()=>{
-    let loadCart;
-    let paymentsummary;
-    beforeEach(()=>{
+describe("PaymmentSummary Component", () => {
+  let loadCart;
+  let paymentsummary;
+  beforeEach(() => {
     loadCart = vi.fn();
-   paymentsummary ={"totalItems":3,"productCostCents":2397,"shippingCostCents":0,"totalCostBeforeTaxCents":2397,"taxCents":240,"totalCostCents":2637}
-})
-    it('search For correct Dollar amount',async()=>{
+    paymentsummary = {
+      totalItems: 3,
+      productCostCents: 2397,
+      shippingCostCents: 0,
+      totalCostBeforeTaxCents: 2397,
+      taxCents: 240,
+      totalCostCents: 2637,
+    };
+    
+  });
+  it("search For correct Dollar amount", async () => {
+    render(
+      <MemoryRouter>
+        <PaymentSummary
+          paymentSummary={paymentsummary}
+          loadCart={loadCart}
+        ></PaymentSummary>
+      </MemoryRouter>
+    );
 
-        render(
-            <MemoryRouter>
-                <PaymentSummary paymentSummary={paymentsummary} loadCart={loadCart}></PaymentSummary>
-            </MemoryRouter>
-        );
-
-        const paymentSummaryrow = await screen.findAllByTestId("payment-summary-row");
-        const payment1 = within(paymentSummaryrow[4]).getByTestId('correct-dollar-amount');
-        expect(payment1).toHaveTextContent('$26.37')
-
-    })
-})
+    const paymentSummaryrow = await screen.findAllByTestId(
+      "payment-summary-row"
+    );
+    const payment1 = within(paymentSummaryrow[4]).getByTestId(
+      "correct-dollar-amount"
+    );
+    expect(payment1).toHaveTextContent("$26.37");
+  });
+  it("click the place order button", () => {});
+});
