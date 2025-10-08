38d3dcf5d6f96fe3c2df46ae6fce2412892de7fd
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-08 23:46:58 +0500

    delete HomeContext and CartContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-// import { StrictMode } from "react";
 import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
-import { CartContext } from "./pages/checkout/CartContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
@@ -10,9 +8,7 @@ import { Provider } from "react-redux";
 createRoot(document.getElementById("root")).render(
   <Provider store ={store}>
     <BrowserRouter>
-      <CartContext>
           <App />
-      </CartContext>
     </BrowserRouter>
   </Provider>
 );
