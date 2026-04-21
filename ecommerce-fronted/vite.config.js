f823996276e69854f7cc8a87e2ea2b7aa7267328
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-04-21 17:21:14 +0500

    feat: Add Product component with quantity selection and cart functionality
    feat: Implement ProductGrid to display products and handle loading state
    
    style: Add responsive styles for homepage and product grid
    
    feat: Create NotFoundPage for handling 404 errors
    
    feat: Implement Order page to display user orders and order details
    
    feat: Add Tracking page for tracking order delivery status
    
    feat: Create Redux slices for cart, orders, and products with async actions
    
    test: Add unit tests for Product component and money formatting utility
    
    chore: Configure Vite for development and testing environment

--- ecommerce-fronted/vite.config.js
+++ ecommerce-fronted/vite.config.js
@@ -11,10 +11,14 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://ecommerce-project-53hw.onrender.com',
+        changeOrigin : true,
+        secure : false,
       },
       '/images':{
-        target:'http://localhost:3000'
+        target:'https://ecommerce-project-53hw.onrender.com',
+        changeOrigin : true,
+        secure : false
       }
     }
   }
