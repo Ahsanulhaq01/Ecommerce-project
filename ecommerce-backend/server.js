3ea75c00e2ff7867b5e50de30eb65689a4dccc94
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-05-03 14:13:13 +0500

    fix: Correct CORS origin URL by removing trailing slash

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors({
-  origin : "https://ecommerce-fronted-pi.vercel.app/",
+  origin : "https://ecommerce-fronted-pi.vercel.app",
 }));
 app.use(express.json());
 
